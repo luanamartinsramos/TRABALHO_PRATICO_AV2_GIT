302a7a82d91f832d49356d6282ff49faf0d8f460
moon-11 <luanamr2222@gmail.com>
2025-12-11 23:29:17 -0300

    Refactor CSV file handling setup in main.py to define CAMPOS list

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ import pandas as pd
 import os
 
 ARQUIVO_CSV = "alunos.csv"
+
+CAMPOS = [
+    "matricula",
+    "nome",
+    "rua",
+    "numero",
+    "bairro",
+    "cidade",
+    "uf",
+    "telefone",
+    "email"
+]
